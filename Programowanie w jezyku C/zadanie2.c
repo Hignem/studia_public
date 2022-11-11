45136c3ec620314e9cf3915c744a56f83089a6d4
Hignem <81192237+Hignem@users.noreply.github.com>
2022-11-12 00:17:50 +0100

    12.11

--- Programowanie w jezyku C/zadanie2.c
+++ Programowanie w jezyku C/zadanie2.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
-int main() 
+void printSquare(char a, int w)
+{   
+    printf("\n\n");
+    for (int i = 0; i < w; i++)
+    {
+        
+        for (int j=0; j < w; j++)
+        {
+            printf("%c", a);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+}
+void opcjaDruga()
+{
+    ;
+}
+void opcjaTrzecia()
+{
+    ;
+}
+
+void menu()
 {
     int indeks = 173600;
     int rokStudiow = 1;
     int wybranaOpcja = -1;
+
     printf ("Program w jezyku C wykonany przez studenta o indeksie nr %d na %d roku studiow\n", indeks, rokStudiow);
     do
     {   
@@ -14,14 +38,23 @@ int main()
         {
             case 1:
                 printf(" \n *** Wybrano opcje %d ***\n \n", wybranaOpcja);
+                printf("Podaj znak: ");
+                char znak;
+                scanf("%c", &znak);
+                printf("\nPodaj szerokosc: ");
+                int szerokosc;
+                scanf("%d", &szerokosc);
+                printSquare(znak, szerokosc);
                 break;
             
             case 2:
                 printf(" \n *** Wybrano opcje %d ***\n \n", wybranaOpcja);
+                opcjaDruga();
                 break;
                 
             case 3:
                 printf(" \n *** Wybrano opcje %d ***\n \n", wybranaOpcja);
+                opcjaTrzecia();
                 break;
             
             case 0:
@@ -34,6 +67,11 @@ int main()
         }
     }
     while(wybranaOpcja!=0);
+}
 
+int main() 
+{
+
+menu();
 
 }
